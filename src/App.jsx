0eaa82c08100cc0c2d68ff54e00fe9cbed387800
jcamsler <jcamsler@gmail.com>
2024-02-21 14:52:56 +0100

    Building player UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <p>Coming soon...</p>
-  )
+    <main>
+      <div id="game-container">
+      <ol id="players">
+        <Player initialName="Player1" symbol="X"/>
+        <Player initialName="Player2" symbol="O"/>        
+      </ol>
+      GAME BOARD
+      </div>
+      LOG
+    </main>);
 }
 
 export default App
---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Player ({initialName, symbol}){
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);  
+
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>
+  //let btnCaption  = 'Edit'
+
+  function handleChange(event){
+    setPlayerName(event.target.value);
+  }
+
+
+  if (isEditing){
+    editablePlayerName = <input type="text" required defaultValue={playerName} onChange={handleChange}/>
+   // btnCaption="Save"
+  }
+  
+  function handleEditClick(){
+    setIsEditing((editing) => !editing);
+  }
+
+  function saveName(){
+    setIsEditing(false);
+    document.getElementById("updatedName").value();
+    setupdatedName();    
+  }
+
+    return ( <li>
+        <span className="player">
+        {editablePlayerName}       
+        <span className="player-symbol">{symbol}</span>
+        </span>
+
+        <button onClick={handleEditClick}>{isEditing ? "Save": "Edit"}</button>
+
+      </li>);
+}
